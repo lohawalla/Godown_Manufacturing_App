94bcd3ea664e2991775dfea1b1281be1df779536
Danish21121998 <100338574+Danish21121998@users.noreply.github.com>
2024-03-19 19:15:13 +0530

    routes

--- src/services/ApiRoutes.ts
+++ src/services/ApiRoutes.ts
@@ -1,5 +1,18 @@
 export const basePath = `https://chawlacomponents.com/`;
 
 export const loginRoutes = `${basePath}api/v1/auth/admin/login`
+
+//salesBillList
 export const salesList = `${basePath}api/v1/godownApp/sales`
+
+//SpecificBillUsingId
 export const salesBill = `${basePath}api/v1/godownApp/sales`
+
+// godown
+export const allGodownsApiPath = `${basePath}api/v1/godownApp/godown`;
+
+// shelf
+export const allShelvesApiPath = `${basePath}api/v1/godownApp/shelf`;
+
+// aisle
+export const allAislesApiPath = `${basePath}api/v1/godownApp/aisle`;
